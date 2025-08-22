9319da0810af31626f85485460f1b3008ccd1b31
GitHub Action <action@github.com>
2025-08-22 10:11:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/arthurbrunet/oraclereservedguard/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/arthurbrunet/oraclereservedguard/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.arthurbrunet.oraclereservedguard.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.arthurbrunet.oraclereservedguard.MyBundle
+import com.github.arthurbrunet.oraclereservedguard.services.MyProjectService
 import javax.swing.JButton
 
 
